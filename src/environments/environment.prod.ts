3a865d47b7719e67fcae1bc8bb26beaa7090f1ed
ajbounifiras <98288245+ajbounifiras@users.noreply.github.com>
2026-02-22 21:35:09 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'healthapp-backend-production-eb8c.up.railway.app'
+  apiUrl: 'https://healthapp-backend-production-eb8c.up.railway.app/api'
 };
